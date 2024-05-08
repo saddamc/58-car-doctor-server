da97270abb32e8b9d42a50c7153f0f9b7da48290
saddamcall <saddam13bd@gmail.com>
2024-05-08 14:00:35 +0600

    finished

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [
+    'http://localhost:5173',
+    'https://cars-doctor-7c7d8.web.app',
+    'https://cars-doctor-7c7d8.firebaseapp.com'
+
+  ],
   credentials: true
 }));
 app.use(express.json());
